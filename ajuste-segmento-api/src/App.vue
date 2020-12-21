7e994f962c3ab2b26e8b7ba830fb57e909c8957e
Yuri Miranda Fialho <yuri@crmpiperun.com>
2020-12-21 17:18:08 -0300

    Ajuste no frontend da aplicação e configuração do Vue

--- ajuste-segmento-api/src/App.vue
+++ ajuste-segmento-api/src/App.vue
@@ -1,13 +1,54 @@
 <template>
     <div id="app">
-        <img alt="PipeRun logo" src="https://crmpiperun.com/wp-content/uploads/2017/04/piperun-crm-vendas-logodark.png">
         <div class="container">
             <div class="row">
-                <button class="btn btn-primary" type="button" @click="getCompanies" :disabled="getButton">Buscar</button>
+                <div class="col-sm-4"></div>
+                <div class="col-sm-4">
+                    <img alt="PipeRun logo" src="https://crmpiperun.com/wp-content/uploads/2017/04/piperun-crm-vendas-logodark.png">
+                </div>
+                <div class="col-sm-4"></div>
+            </div>
+            <div style="padding-top:50px;"></div>
+            <div class="row">
+                <div class="col-sm-4"></div>
+                <div class="col-sm-4">
+                    <div class="well" style="text-align: left">
+                        <p>
+                            Este aplicativo faz a busca de todas as empresas do cliente, retira o atributo 
+                            segmento das empresas e remove <strong>todos</strong> os segmentos da base.
+                            <br><br>
+                            Para que funcione, é importante informar o token da API do cliente.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-sm-4"></div>
+            </div>
+            <div style="padding-top:50px;"></div>
+            <div class="row">
+                <div class="col-sm-4"></div>
+                <div class="col-sm-4">
+                    <p>Buscar todas as empresas.</p>
+                    <button class="btn btn-primary btn-lg btn-block" type="button" @click="getCompanies" :disabled="getButton">Buscar</button>
+                </div>
+                <div class="col-sm-4"></div>
             </div>
             <div style="padding-top:25px;"></div>
             <div class="row">
-                <button class="btn btn-warning" type="button" @click="updateCompanies" :disabled="updateButton">Atualizar</button>
+                <div class="col-sm-4"></div>
+                <div class="col-sm-4">
+                    <p>Remover o atributo <strong>segmento</strong> das empresas.</p>
+                    <button class="btn btn-warning btn-lg btn-block" type="button" @click="updateCompanies" :disabled="updateButton">Atualizar</button>
+                </div>
+                <div class="col-sm-4"></div>
+            </div>
+            <div style="padding-top:25px;"></div>
+            <div class="row">
+                <div class="col-sm-4"></div>
+                <div class="col-sm-4">
+                    <p>Remover <strong>todos</strong> os segmentos.</p>
+                    <button class="btn btn-danger btn-lg btn-block" type="button" @click="deleteSegments" :disabled="deleteButton">Remover</button>
+                </div>
+                <div class="col-sm-4"></div>
             </div>
         </div>
     </div>
@@ -27,6 +68,7 @@ export default {
             page: 1,
             getButton: true,
             updateButton: true,
+            deleteButton: true
         }
     },
     methods: {
@@ -46,6 +88,23 @@ export default {
             }
         },
         updateCompanies() {
+            if (this.companies.length == this.total) {
+                this.companies.forEach(element => {
+                    http.put('/companies/' + element.id, {
+                            'segment_id': null
+                        })
+                        .then(response => {
+                            console.log(response)
+                        })
+                        .catch(e => {
+                            console.log(e)
+                        })
+                })
+            } else {
+                alert('Problema ao atualizar empresas. A quantidade total não foi totalmente carregada.')
+            }
+        },
+        deleteSegments() {
 
         }
     },
@@ -68,7 +127,6 @@ export default {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
     color: #2c3e50;
     margin-top: 60px;
 }
