779266e29b952727fefdd4fb5aa40d78fdf8597a
Yuri Miranda Fialho <yuri@crmpiperun.com>
2020-12-23 09:00:34 -0300

    Ajustes no layout

--- ajuste-segmento-api/src/App.vue
+++ ajuste-segmento-api/src/App.vue
@@ -2,62 +2,89 @@
     <div id="app">
         <div class="container">
             <div class="row">
-                <div class="col-sm-4"></div>
-                <div class="col-sm-4">
-                    <img alt="PipeRun logo" src="https://crmpiperun.com/wp-content/uploads/2017/04/piperun-crm-vendas-logodark.png">
-                </div>
-                <div class="col-sm-4"></div>
-            </div>
-            <div style="padding-top:50px;"></div>
-            <div class="row">
-                <div class="col-sm-4"></div>
-                <div class="col-sm-4">
+                <div class="col-sm-6">
                     <div class="well" style="text-align: left">
+                        <div style="text-align: center; margin-bottom: 25px;">
+                            <img alt="PipeRun logo" src="https://piperun.s3-sa-east-1.amazonaws.com/logo/logo_preta.png" width="200px">
+                        </div>
                         <p>
                             Este aplicativo faz a busca de todas as empresas do cliente, retira o atributo 
                             segmento das empresas e remove <strong>todos</strong> os segmentos da base.
                             <br><br>
                             Para que funcione, é importante informar o token da API do cliente.
                         </p>
+                        <div class="form-group">
+                            <input class="form-control" type="text" value="" v-model="token" />
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-success btn-lg btn-block" @click="begin" :disabled="get_button">Iniciar Processo</button>
+                        </div>
                     </div>
                 </div>
-                <div class="col-sm-4"></div>
-            </div>
-            <div style="padding-top:50px;"></div>
-            <div class="row">
-                <div class="col-sm-4"></div>
-                <div class="col-sm-4">
-                    <p>Buscar todas as empresas.</p>
-                    <button class="btn btn-primary btn-lg btn-block" type="button" @click="getCompanies" :disabled="getButton">Buscar</button>
+                <div class="col-sm-6">
+                    <div class="well">
+                        <div :class="['alert ', (prog_get_companies == 100) ? 'alert-success' : 'alert-warning']">
+                            <div class="row">
+                                <div class="col-sm-1">
+                                    <i :class="['glyphicon', (prog_get_companies == 100) ? 'glyphicon-ok-circle' : (prog_get_companies == 0) ? 'glyphicon-time' : 'glyphicon-spin glyphicon-refresh']"></i>
+                                </div>
+                                <div class="col-sm-11">
+                                    <p>Buscar todas as empresas.</p>
+                                    <div class="progress" v-show="show_get_companies">
+                                        <div :class="['progress-bar', (prog_get_companies == 100) ? 'progress-bar-success' : 'progress-bar-warning progress-bar-striped active']" role="progressbar" :aria-valuenow="prog_get_companies" aria-valuemin="0" aria-valuemax="100" :style="{ width: prog_get_companies + '%' }">
+                                            {{ prog_get_companies }} %
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="alert alert-warning">
+                            <div class="row">
+                                <div class="col-sm-1">
+                                    <i class="glyphicon glyphicon-time"></i>
+                                </div>
+                                <div class="col-sm-11">
+                                    <p>Remover o atributo <strong>segmento</strong> das empresas.</p>
+                                    <div class="progress" v-show="show_update_companies">
+                                        <div class="progress-bar" role="progressbar" :aria-valuenow="prog_update_companies" aria-valuemin="0" aria-valuemax="100" :style="{ width: prog_update_companies + '%' }">
+                                            {{ prog_update_companies }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="alert alert-warning">
+                            <div class="row">
+                                <div class="col-sm-1">
+                                    <i class="glyphicon glyphicon-time"></i>
+                                </div>
+                                <div class="col-sm-11">
+                                    <p>Buscar todas os segmentos.</p>
+                                    <div class="progress" v-show="show_get_segments">
+                                        <div class="progress-bar" role="progressbar" :aria-valuenow="prog_get_segments" aria-valuemin="0" aria-valuemax="100" :style="{ width: prog_get_segments + '%' }">
+                                            {{ prog_get_segments }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="alert alert-warning">
+                            <div class="row">
+                                <div class="col-sm-1">
+                                    <i class="glyphicon glyphicon-time"></i>
+                                </div>
+                                <div class="col-sm-11">
+                                    <p>Remover <strong>todos</strong> os segmentos.</p>
+                                    <div class="progress" v-show="show_delete_segments">
+                                        <div class="progress-bar" role="progressbar" :aria-valuenow="prog_delete_segments" aria-valuemin="0" aria-valuemax="100" :style="{ width: prog_delete_segments + '%' }">
+                                            {{ prog_delete_segments }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-sm-4"></div>
-            </div>
-            <div style="padding-top:25px;"></div>
-            <div class="row">
-                <div class="col-sm-4"></div>
-                <div class="col-sm-4">
-                    <p>Remover o atributo <strong>segmento</strong> das empresas.</p>
-                    <button class="btn btn-warning btn-lg btn-block" type="button" @click="updateCompanies">Atualizar</button>
-                </div>
-                <div class="col-sm-4"></div>
-            </div>
-            <div style="padding-top:25px;"></div>
-            <div class="row">
-                <div class="col-sm-4"></div>
-                <div class="col-sm-4">
-                    <p>Buscar todas os segmentos.</p>
-                    <button class="btn btn-primary btn-lg btn-block" type="button" @click="getSegments" :disabled="getButton">Buscar</button>
-                </div>
-                <div class="col-sm-4"></div>
-            </div>
-            <div style="padding-top:25px;"></div>
-            <div class="row">
-                <div class="col-sm-4"></div>
-                <div class="col-sm-4">
-                    <p>Remover <strong>todos</strong> os segmentos.</p>
-                    <button class="btn btn-danger btn-lg btn-block" type="button" @click="deleteSegments" :disabled="deleteButton">Remover</button>
-                </div>
-                <div class="col-sm-4"></div>
             </div>
         </div>
     </div>
@@ -77,18 +104,39 @@ export default {
             companies: [],
             segments: [],
             page: 0,
-            getButton: true,
-            updateButton: true,
-            deleteButton: true
+            token: 'a3f1c21d0824e5a6406c3f6e18885dbe',
+            //token: '62a0b768c257014f4382f270dc9bd198',
+            actual_percentage: 0,
+            prog_get_companies: 0,
+            prog_get_segments: 0,
+            prog_update_companies: 0,
+            prog_delete_segments: 0,
+            show_get_companies: false,
+            show_get_segments: false,
+            show_update_companies: false,
+            show_delete_segments: false,
+            get_button: true,
+            update_button: true,
+            delete_button: true
         }
     },
     methods: {
+        begin() {
+            http.defaults.headers.token = this.token
+
+            this.getCompanies()
+        },
         getCompanies() {
             this.page = 1
+            this.actual_percentage = 1
 
             while (this.page <= this.total_pages_companies) {
                 http.get('/companies?show=200&page=' + this.page)
                     .then(response => {
+                        this.show_get_companies = true
+                        this.prog_get_companies = Math.floor(this.actual_percentage / this.total_pages_companies * 100)
+                        this.actual_percentage++
+
                         response.data.data.forEach(element => {
                             this.companies.push(element.id)
                         });
@@ -101,12 +149,18 @@ export default {
             }
         },
         updateCompanies() {
+            this.actual_percentage = 1
+
             if (this.companies.length == this.total_companies) {
                 for (let i = 0; i < this.companies.length; i++) {
                     http.put('/companies/' + this.companies[i], {
                             'segment_id': null
                         })
                         .then(response => {
+                            this.show_update_companies = true
+                            this.prog_update_companies = Math.floor(this.actual_percentage / this.total_companies * 100)
+                            this.actual_percentage++
+
                             console.log(response)
                         })
                         .catch(e => {
@@ -119,10 +173,15 @@ export default {
         },
         getSegments() {
             this.page = 1
-            
+            this.actual_percentage = 1
+
             while (this.page <= this.total_pages_segments) {
                 http.get('/segments?show=200&page=' + this.page)
                     .then(response => {
+                        this.show_get_segments = true
+                        this.prog_get_segments = Math.floor(this.actual_percentage / this.total_pages_segments * 100)
+                        this.actual_percentage++
+
                         response.data.data.forEach(element => {
                             this.segments.push(element.id)
                         });
@@ -135,10 +194,16 @@ export default {
             }
         },
         deleteSegments() {
+            this.actual_percentage = 1
+
             if (this.segments.length == this.total_segments) {
                 for (let i = 0; i < this.segments.length; i++) {
                     http.delete('/segments/' + this.segments[i])
                         .then(response => {
+                            this.show_delete_segments = true
+                            this.prog_delete_segments = Math.floor(this.actual_percentage / this.total_segments * 100)
+                            this.actual_percentage++
+
                             console.log(response)
                         })
                         .catch(e => {
@@ -151,21 +216,21 @@ export default {
         }
     },
     async mounted() {
-        http.get('/companies?show=200&page=0')
+        http.get('/companies?show=200&page=0&token=' + this.token)
             .then(response => {
                 this.total_companies = response.data.meta.total
                 this.total_pages_companies = response.data.meta.total_pages
-                this.getButton = false
+                this.get_button = false
             })
             .catch(e => {
                 console.log(e)
             })
 
-        http.get('/segments?show=200&page=0')
+        http.get('/segments?show=200&page=0&token=' + this.token)
             .then(response => {
                 this.total_segments = response.data.meta.total
                 this.total_pages_segments = response.data.meta.total_pages
-                this.deleteButton = false
+                this.delete_button = false
             })
             .catch(e => {
                 console.log(e)
@@ -181,5 +246,31 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
     margin-top: 60px;
+}
+
+.glyphicon-spin {
+    -webkit-animation: spin 1000ms infinite linear;
+    animation: spin 1000ms infinite linear;
+}
+
+@-webkit-keyframes spin {
+    0% {
+        -webkit-transform: rotate(0deg);
+        transform: rotate(0deg);
+    }
+    100% {
+        -webkit-transform: rotate(359deg);
+        transform: rotate(359deg);
+    }
+}
+@keyframes spin {
+    0% {
+        -webkit-transform: rotate(0deg);
+        transform: rotate(0deg);
+    }
+    100% {
+        -webkit-transform: rotate(359deg);
+        transform: rotate(359deg);
+    }
 }
 </style>
--- ajuste-segmento-api/src/plugins/axios.js
+++ ajuste-segmento-api/src/plugins/axios.js
@@ -8,9 +8,7 @@ Vue.use(axios)
 export default rateLimit(axios.create({
         baseURL: 'https://api.pipe.run/v1',
         headers: {
-            'Content-Type': 'application/json',
-            //'token': 'a3f1c21d0824e5a6406c3f6e18885dbe'
-            'token': '62a0b768c257014f4382f270dc9bd198'
+            'Content-Type': 'application/json'
         }
     }), {
         maxRequests: 2,
