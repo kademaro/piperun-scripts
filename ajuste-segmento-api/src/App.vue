e715461cc11fe3b446f51993369427896030c7cb
Yuri Miranda Fialho <yuri@crmpiperun.com>
2020-12-23 14:56:10 -0300

    - Instalado e implementado o SweetAlert2 - Ajustes de validação do Token, API fora e correções no encadeamento de funções.

--- ajuste-segmento-api/src/App.vue
+++ ajuste-segmento-api/src/App.vue
@@ -14,10 +14,10 @@
                             Para que funcione, é importante informar o token da API do cliente.
                         </p>
                         <div class="form-group">
-                            <input class="form-control" type="text" value="" v-model="token" />
+                            <input class="form-control" type="text" value="" v-model="token" :disabled="token_input" />
                         </div>
                         <div class="form-group">
-                            <button class="btn btn-success btn-lg btn-block" @click="begin" :disabled="get_button">Iniciar Processo</button>
+                            <button class="btn btn-success btn-lg btn-block" @click="begin" :disabled="!token">Iniciar Processo</button>
                         </div>
                     </div>
                 </div>
@@ -38,7 +38,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="alert alert-warning">
+                        <div :class="['alert ', (prog_update_companies == 100) ? 'alert-success' : 'alert-warning']">
                             <div class="row">
                                 <div class="col-sm-1">
                                     <i class="glyphicon glyphicon-time"></i>
@@ -46,14 +46,14 @@
                                 <div class="col-sm-11">
                                     <p>Remover o atributo <strong>segmento</strong> das empresas.</p>
                                     <div class="progress" v-show="show_update_companies">
-                                        <div class="progress-bar" role="progressbar" :aria-valuenow="prog_update_companies" aria-valuemin="0" aria-valuemax="100" :style="{ width: prog_update_companies + '%' }">
-                                            {{ prog_update_companies }}
+                                        <div :class="['progress-bar', (prog_update_companies == 100) ? 'progress-bar-success' : 'progress-bar-warning progress-bar-striped active']" role="progressbar" :aria-valuenow="prog_update_companies" aria-valuemin="0" aria-valuemax="100" :style="{ width: prog_update_companies + '%' }">
+                                            {{ prog_update_companies }} %
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="alert alert-warning">
+                        <div :class="['alert ', (prog_get_segments == 100) ? 'alert-success' : 'alert-warning']">
                             <div class="row">
                                 <div class="col-sm-1">
                                     <i class="glyphicon glyphicon-time"></i>
@@ -61,14 +61,14 @@
                                 <div class="col-sm-11">
                                     <p>Buscar todas os segmentos.</p>
                                     <div class="progress" v-show="show_get_segments">
-                                        <div class="progress-bar" role="progressbar" :aria-valuenow="prog_get_segments" aria-valuemin="0" aria-valuemax="100" :style="{ width: prog_get_segments + '%' }">
-                                            {{ prog_get_segments }}
+                                        <div :class="['progress-bar', (prog_get_segments == 100) ? 'progress-bar-success' : 'progress-bar-warning progress-bar-striped active']" role="progressbar" :aria-valuenow="prog_get_segments" aria-valuemin="0" aria-valuemax="100" :style="{ width: prog_get_segments + '%' }">
+                                            {{ prog_get_segments }} %
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="alert alert-warning">
+                        <div :class="['alert ', (prog_delete_segments == 100) ? 'alert-success' : 'alert-warning']">
                             <div class="row">
                                 <div class="col-sm-1">
                                     <i class="glyphicon glyphicon-time"></i>
@@ -76,8 +76,8 @@
                                 <div class="col-sm-11">
                                     <p>Remover <strong>todos</strong> os segmentos.</p>
                                     <div class="progress" v-show="show_delete_segments">
-                                        <div class="progress-bar" role="progressbar" :aria-valuenow="prog_delete_segments" aria-valuemin="0" aria-valuemax="100" :style="{ width: prog_delete_segments + '%' }">
-                                            {{ prog_delete_segments }}
+                                        <div :class="['progress-bar', (prog_delete_segments == 100) ? 'progress-bar-success' : 'progress-bar-warning progress-bar-striped active']" role="progressbar" :aria-valuenow="prog_delete_segments" aria-valuemin="0" aria-valuemax="100" :style="{ width: prog_delete_segments + '%' }">
+                                            {{ prog_delete_segments }} %
                                         </div>
                                     </div>
                                 </div>
@@ -92,6 +92,7 @@
 
 <script>
 import http from './plugins/axios'
+import SweetAlert from 'sweetalert2'
 
 export default {
     name: 'App',
@@ -104,8 +105,8 @@ export default {
             companies: [],
             segments: [],
             page: 0,
-            token: 'a3f1c21d0824e5a6406c3f6e18885dbe',
-            //token: '62a0b768c257014f4382f270dc9bd198',
+            token: '',
+            token_input: false,
             actual_percentage: 0,
             prog_get_companies: 0,
             prog_get_segments: 0,
@@ -114,17 +115,122 @@ export default {
             show_get_companies: false,
             show_get_segments: false,
             show_update_companies: false,
-            show_delete_segments: false,
-            get_button: true,
-            update_button: true,
-            delete_button: true
+            show_delete_segments: false
         }
     },
     methods: {
         begin() {
             http.defaults.headers.token = this.token
+            this.actual_percentage = 1
 
-            this.getCompanies()
+            SweetAlert.fire({   
+                title: 'Você tem certeza disso?',
+                text: "Você não terá a possibilidade de reverter o processo!",
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sim',
+                cancelButtonText: 'Não',
+                customClass: {
+                    confirmButton: 'btn btn-success',
+                    cancelButton: 'btn btn-danger',
+                },
+                buttonsStyling: false
+            }).then((result) => {
+                if (result.isDismissed) {
+                    SweetAlert.fire({
+                        title: 'Cancelado',
+                        text: 'O processo foi cancelado com sucesso',
+                        icon: 'info',
+                        confirmButtonColor: '#3085d6',
+                        customClass: {
+                            confirmButton: 'btn btn-success'
+                        },
+                        buttonsStyling: false
+                    })
+                } else {
+                    http.get('/companies?show=200&page=0')
+                        .then(response => {
+                            this.total_companies = response.data.meta.total
+                            this.total_pages_companies = response.data.meta.total_pages
+
+                            if (response.status == 200) {
+                                this.token_input = true
+                                return this.getCompanies()
+                            }
+                        })
+                        .catch(error => {
+                            switch (error.response.status) {
+                                case 401:
+                                    SweetAlert.fire({
+                                        title: 'Acesso Não Autorizado',
+                                        text: 'Verifique o token inserido e tente novamente.',
+                                        icon: 'info',
+                                        confirmButtonColor: '#3085d6',
+                                        customClass: {
+                                            confirmButton: 'btn btn-success'
+                                        },
+                                        buttonsStyling: false
+                                    })
+                                    break
+                                case 503:
+                                    SweetAlert.fire({
+                                        title: 'Sistema Indisponível',
+                                        text: 'A API do PipeRun está fora do ar. Tente novamente mais tarde.',
+                                        icon: 'info',
+                                        confirmButtonColor: '#3085d6',
+                                        customClass: {
+                                            confirmButton: 'btn btn-success'
+                                        },
+                                        buttonsStyling: false
+                                    })
+                                    break
+                                default:
+                                    console.log(error)
+                                    break
+                            }
+                        })
+        
+                    http.get('/segments?show=200&page=0')
+                        .then(response => {
+                            this.total_segments = response.data.meta.total
+                            this.total_pages_segments = response.data.meta.total_pages
+                        })
+                        .catch(error => {
+                            switch (error.response.status) {
+                                case 401:
+                                    SweetAlert.fire({
+                                        title: 'Acesso Não Autorizado',
+                                        text: 'Verifique o token inserido e tente novamente.',
+                                        icon: 'info',
+                                        confirmButtonColor: '#3085d6',
+                                        customClass: {
+                                            confirmButton: 'btn btn-success'
+                                        },
+                                        buttonsStyling: false
+                                    })
+                                    break
+                                case 503:
+                                    SweetAlert.fire({
+                                        title: 'Sistema Indisponível',
+                                        text: 'A API do PipeRun está fora do ar. Tente novamente mais tarde.',
+                                        icon: 'info',
+                                        confirmButtonColor: '#3085d6',
+                                        customClass: {
+                                            confirmButton: 'btn btn-success'
+                                        },
+                                        buttonsStyling: false
+                                    })
+                                    break
+                                default:
+                                    console.log(error)
+                                    break
+                            }
+                        })
+                }
+            })
+            
         },
         getCompanies() {
             this.page = 1
@@ -140,6 +246,10 @@ export default {
                         response.data.data.forEach(element => {
                             this.companies.push(element.id)
                         });
+
+                        if (response.status == 200 && this.prog_get_companies == 100) {
+                            return this.updateCompanies()
+                        }
                     })
                     .catch(e => {
                         console.log(e)
@@ -161,6 +271,10 @@ export default {
                             this.prog_update_companies = Math.floor(this.actual_percentage / this.total_companies * 100)
                             this.actual_percentage++
 
+                            if (this.prog_update_companies == 100) {
+                                this.getSegments()
+                            }
+
                             console.log(response)
                         })
                         .catch(e => {
@@ -168,7 +282,15 @@ export default {
                         })
                 }
             } else {
-                alert('Problema ao atualizar empresas. A quantidade total não foi totalmente carregada.')
+                SweetAlert.fire({
+                    title: 'Problema ao atualizar empresas',
+                    text: 'A quantidade total não foi totalmente carregada.',
+                    icon: 'error',
+                    customClass: {
+                        confirmButton: 'btn btn-danger'
+                    },
+                    buttonsStyling: false
+                })
             }
         },
         getSegments() {
@@ -185,6 +307,10 @@ export default {
                         response.data.data.forEach(element => {
                             this.segments.push(element.id)
                         });
+
+                        if (response.status == 200 && this.prog_get_segments == 100) {
+                            this.deleteSegments()
+                        }
                     })
                     .catch(e => {
                         console.log(e)
@@ -211,31 +337,27 @@ export default {
                         })
                 }
             } else {
-                alert('Problema ao atualizar empresas. A quantidade total não foi totalmente carregada.')
+                SweetAlert.fire({
+                    title: 'Problema ao remover os segmentos',
+                    text: 'A quantidade total não foi totalmente carregada.',
+                    icon: 'error',
+                    customClass: {
+                        confirmButton: 'btn btn-danger'
+                    },
+                    buttonsStyling: false
+                })
             }
+
+            this.token_input = false
+            SweetAlert.fire('Ajuste Segmento API', '<p>O processo terminou com sucesso!</p>', 'success');
         }
     },
-    async mounted() {
-        http.get('/companies?show=200&page=0&token=' + this.token)
-            .then(response => {
-                this.total_companies = response.data.meta.total
-                this.total_pages_companies = response.data.meta.total_pages
-                this.get_button = false
-            })
-            .catch(e => {
-                console.log(e)
-            })
-
-        http.get('/segments?show=200&page=0&token=' + this.token)
-            .then(response => {
-                this.total_segments = response.data.meta.total
-                this.total_pages_segments = response.data.meta.total_pages
-                this.delete_button = false
-            })
-            .catch(e => {
-                console.log(e)
-            })
-    }
+    // events: {
+    //     'ready.begin': function (key) {
+    //         console.log('Disparei o evento do Begin')
+    //         this.getCompanies(key)
+    //     }   
+    // }
 }
 </script>
 
