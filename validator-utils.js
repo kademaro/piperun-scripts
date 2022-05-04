e20147071e412d2f5af6a8a89e7eaabb2354b2c1
Osvaldo Gehm <osvaldo@odig.net>
2022-05-04 17:15:30 -0300

    Create validator-utils.js

---
+++ validator-utils.js
@@ -0,0 +1,23 @@
+function validateForm(form) {
+    let errors_count = 0
+    Object.entries(form.querySelectorAll('input')).map( (input) => {
+        if (input[1].type !== 'hidden') {
+            if(input[1].hasAttribute('required') && input[1].value.length < 3) {
+                errors_count ++;
+            }
+            
+        }
+    });
+
+  if (errors_count > 0){
+      return false; 
+  }
+    
+  return true;
+}
+
+function validateEmail(email) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
